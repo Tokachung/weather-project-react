d8f922d32ed0484e89ba6257656d8cd1ab8c663e
Tokachung <st1403@live.com>
2022-08-04 21:18:36 +1200

    New project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cb3a8590a21f7cf52d5e9d727d871959`;
+
+  return <div></div>;
+}
+
+export default App;
