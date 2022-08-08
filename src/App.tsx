a4662ba84149e7828233b886b2bcc184177e6268
Tokachung <st1403@live.com>
2022-08-08 14:46:36 +1200

    cleaning up unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,16 @@ function App() {
   const [value, setValue] = useState();
 
   async function getWeatherUpdate() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cb3a8590a21f7cf52d5e9d727d871959`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cb3a8590a21f7cf52d5e9d727d871959&units=metric`;
     const response = await axios.get(url);
     setData(response.data);
   }
 
   return (
-    <Box
-      sx={{
-        display: "table",
-        height: "100px",
-        width: "100%",
-        textAlign: "center",
-      }}
-    >
-      <Box
-        sx={{
-          display: "table-cell",
-          verticalAlign: "middle",
-        }}
-      >
-        <h1>My Weather App</h1>
-        <Input
+    <div className="app">
+      <div className="search">
+        <input
+          type="text"
           placeholder="Input City Name:"
           value={location}
           onChange={(event) => {
@@ -41,10 +29,51 @@ function App() {
         <Button variant="contained" onClick={getWeatherUpdate}>
           Click Me
         </Button>
-        <Box>{data ? "Country: " + data.sys.country : ""}</Box>
-        <Box>{data ? "Temperature: " + data.main.temp + " Celcius" : ""}</Box>
-      </Box>
-    </Box>
+      </div>
+
+      <div className="container">
+        <div className="top"></div>
+
+        <div className="location">
+          <p>
+            <Box> Country: {data ? data.sys.country : ""}</Box>
+          </p>
+          <div className="temp">
+            <h1>
+              <Box>{data ? +data.main.temp + " °C" : ""}</Box>
+            </h1>
+          </div>
+
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <p>
+              <Box className="bold">
+                {data ? data.main.feels_like + "°C" : ""}
+              </Box>
+            </p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">
+              <Box className="bold">{data ? data.main.humidity + "%" : ""}</Box>
+            </p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">
+              {" "}
+              <Box>{data ? data.wind.speed + "km/hr" : ""}</Box>
+            </p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
